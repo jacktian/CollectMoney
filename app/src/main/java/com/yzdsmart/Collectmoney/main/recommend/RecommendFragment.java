5b312fb7e78c874db9b2b641d96f7796ff17e6be
JacksonLi <libin_cxf@163.com>
2016-08-17 19:05:06 +0800

    主界面

---
+++ app/src/main/java/com/yzdsmart/Collectmoney/main/recommend/RecommendFragment.java
@@ -0,0 +1,14 @@
+package com.yzdsmart.Collectmoney.main.recommend;
+
+import com.yzdsmart.Collectmoney.BaseFragment;
+import com.yzdsmart.Collectmoney.R;
+
+/**
+ * Created by YZD on 2016/8/17.
+ */
+public class RecommendFragment extends BaseFragment {
+    @Override
+    public int getLayoutResource() {
+        return R.layout.fragment_recommend;
+    }
+}
