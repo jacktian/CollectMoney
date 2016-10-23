584bdb932a391e62544797b2d140544fcdde1f3e
JacksonLi-LiBin <libin_cxf@163.com>
2016-10-23 20:04:48 +0800

    更新百度地图

--- app/src/main/java/com/yzdsmart/Collectmoney/main/find_money/FindMoneyFragment.java
+++ app/src/main/java/com/yzdsmart/Collectmoney/main/find_money/FindMoneyFragment.java
@@ -35,6 +35,8 @@ import com.baidu.mapapi.search.core.RouteLine;
 import com.baidu.mapapi.search.core.SearchResult;
 import com.baidu.mapapi.search.route.BikingRouteResult;
 import com.baidu.mapapi.search.route.DrivingRouteResult;
+import com.baidu.mapapi.search.route.IndoorRouteResult;
+import com.baidu.mapapi.search.route.MassTransitRouteResult;
 import com.baidu.mapapi.search.route.OnGetRoutePlanResultListener;
 import com.baidu.mapapi.search.route.PlanNode;
 import com.baidu.mapapi.search.route.RoutePlanSearch;
@@ -410,9 +412,17 @@ public class FindMoneyFragment extends BaseFragment implements FindMoneyContract
         mLocClient = new LocationClient(getActivity());
         mLocClient.registerLocationListener(locListener);
         LocationClientOption option = new LocationClientOption();
-        option.setOpenGps(true); // 打开gps
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
         option.setCoorType("bd09ll"); //返回的定位结果是百度经纬度,默认值gcj02
         option.setScanSpan(LOC_TIME);//设置发起定位请求的间隔时间
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true); // 打开gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
         mLocClient.setLocOption(option);
         mLocClient.start();
     }
@@ -511,7 +521,17 @@ public class FindMoneyFragment extends BaseFragment implements FindMoneyContract
     }
 
     @Override
+    public void onGetMassTransitRouteResult(MassTransitRouteResult massTransitRouteResult) {
+
+    }
+
+    @Override
     public void onGetDrivingRouteResult(DrivingRouteResult drivingRouteResult) {
+
+    }
+
+    @Override
+    public void onGetIndoorRouteResult(IndoorRouteResult indoorRouteResult) {
 
     }
 
