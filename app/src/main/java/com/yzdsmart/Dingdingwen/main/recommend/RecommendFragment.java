9cda68e7fa32d5993e741b0780997cb37f87b1c8
JacksonLi <libin_cxf@163.com>
2016-12-02 16:50:45 +0800

    推荐

--- app/src/main/java/com/yzdsmart/Dingdingwen/main/recommend/RecommendFragment.java
+++ app/src/main/java/com/yzdsmart/Dingdingwen/main/recommend/RecommendFragment.java
@@ -2,79 +2,72 @@ package com.yzdsmart.Dingdingwen.main.recommend;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.webkit.WebView;
 import android.widget.TextView;
 
-import com.marshalchen.ultimaterecyclerview.UltimateRecyclerView;
 import com.umeng.analytics.MobclickAgent;
 import com.yzdsmart.Dingdingwen.BaseActivity;
 import com.yzdsmart.Dingdingwen.BaseFragment;
 import com.yzdsmart.Dingdingwen.R;
-import com.yzdsmart.Dingdingwen.http.response.ExpandListRequestResponse;
-import com.yzdsmart.Dingdingwen.main.MainActivity;
-import com.yzdsmart.Dingdingwen.utils.SharedPreferencesUtils;
-import com.yzdsmart.Dingdingwen.utils.Utils;
-import com.yzdsmart.Dingdingwen.views.BetterSpinner;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Optional;
 
 /**
  * Created by YZD on 2016/8/17.
  */
-public class RecommendFragment extends BaseFragment implements RecommendContract.RecommendView {
+public class RecommendFragment extends BaseFragment {
+    //    implements RecommendContract.RecommendView
     @Nullable
     @BindViews({R.id.title_left_operation_layout, R.id.left_title, R.id.title_logo, R.id.title_right_operation})
     List<View> hideViews;
     @Nullable
     @BindView(R.id.center_title)
     TextView centerTitleTV;
+    //    @Nullable
+//    @BindViews({R.id.city_recommend_spinner})
+//    List<View> showViews;
+//    @Nullable
+//    @BindView(R.id.city_recommend_spinner)
+//    BetterSpinner cityRecommendSpinner;
+//    @Nullable
+//    @BindView(R.id.recommend_list)
+//    UltimateRecyclerView recommendListRV;
     @Nullable
-    @BindViews({R.id.city_recommend_spinner})
-    List<View> showViews;
-    @Nullable
-    @BindView(R.id.city_recommend_spinner)
-    BetterSpinner cityRecommendSpinner;
-    @Nullable
-    @BindView(R.id.recommend_list)
-    UltimateRecyclerView recommendListRV;
+    @BindView(R.id.recommend_webview)
+    WebView recommendWebview;
 
     private static final String TAG = "RecommendFragment";
 
-    private Integer pageIndex = 1;
-    private static final Integer PAGE_SIZE = 10;
-
-    private RecommendContract.RecommendPresenter mPresenter;
-
-    private String[] cities;
-    private ArrayAdapter<String> citiesAdapter;
-
-    private LinearLayoutManager mLinearLayoutManager;
-    private List<ExpandListRequestResponse> expandList;
-    private RecommendAdapter recommendAdapter;
+//    private Integer pageIndex = 1;
+//    private static final Integer PAGE_SIZE = 10;
+//
+//    private RecommendContract.RecommendPresenter mPresenter;
+//
+//    private String[] cities;
+//    private ArrayAdapter<String> citiesAdapter;
+//
+//    private LinearLayoutManager mLinearLayoutManager;
+//    private List<ExpandListRequestResponse> expandList;
+//    private RecommendAdapter recommendAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        new RecommendPresenter(getActivity(), this);
-
-        cities = getResources().getStringArray(R.array.city_list);
-        citiesAdapter = new ArrayAdapter<String>(getActivity(),
-                R.layout.cities_list_item, cities);
-
-        mLinearLayoutManager = new LinearLayoutManager(getActivity());
-        expandList = new ArrayList<ExpandListRequestResponse>();
-        recommendAdapter = new RecommendAdapter(getActivity());
+//        new RecommendPresenter(getActivity(), this);
+//
+//        cities = getResources().getStringArray(R.array.city_list);
+//        citiesAdapter = new ArrayAdapter<String>(getActivity(),
+//                R.layout.cities_list_item, cities);
+//
+//        mLinearLayoutManager = new LinearLayoutManager(getActivity());
+//        expandList = new ArrayList<ExpandListRequestResponse>();
+//        recommendAdapter = new RecommendAdapter(getActivity());
     }
 
     @Override
@@ -87,44 +80,47 @@ public class RecommendFragment extends BaseFragment implements RecommendContract
         super.onActivityCreated(savedInstanceState);
 
         ButterKnife.apply(hideViews, ((BaseActivity) getActivity()).BUTTERKNIFEGONE);
-        ButterKnife.apply(showViews, ((BaseActivity) getActivity()).BUTTERKNIFEVISIBLE);
+//        ButterKnife.apply(showViews, ((BaseActivity) getActivity()).BUTTERKNIFEVISIBLE);
         centerTitleTV.setText(getActivity().getResources().getString(R.string.recommend));
 //        titleRightOpeTLIV.setImageDrawable(getActivity().getResources().getDrawable(R.mipmap.grey_mail_icon));
 
-        cityRecommendSpinner.setAdapter(citiesAdapter);
-        cityRecommendSpinner.setText(cities[0]);
+//        cityRecommendSpinner.setAdapter(citiesAdapter);
+//        cityRecommendSpinner.setText(cities[0]);
+//
+//        recommendListRV.setHasFixedSize(true);
+//        recommendListRV.setLayoutManager(mLinearLayoutManager);
+//        recommendListRV.setAdapter(recommendAdapter);
+//        recommendListRV.reenableLoadmore();
+//        recommendListRV.setOnLoadMoreListener(new UltimateRecyclerView.OnLoadMoreListener() {
+//            @Override
+//            public void loadMore(int itemsCount, int maxLastVisiblePosition) {
+//                getExpandList();
+//            }
+//        });
+//        recommendListRV.setDefaultOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+//            @Override
+//            public void onRefresh() {
+//                recommendListRV.setRefreshing(false);
+//                recommendListRV.reenableLoadmore();
+//                pageIndex = 1;
+//                recommendAdapter.clearList();
+//                getExpandList();
+//            }
+//        });
+//
+//        getExpandList();
 
-        recommendListRV.setHasFixedSize(true);
-        recommendListRV.setLayoutManager(mLinearLayoutManager);
-        recommendListRV.setAdapter(recommendAdapter);
-        recommendListRV.reenableLoadmore();
-        recommendListRV.setOnLoadMoreListener(new UltimateRecyclerView.OnLoadMoreListener() {
-            @Override
-            public void loadMore(int itemsCount, int maxLastVisiblePosition) {
-                getExpandList();
-            }
-        });
-        recommendListRV.setDefaultOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                recommendListRV.setRefreshing(false);
-                recommendListRV.reenableLoadmore();
-                pageIndex = 1;
-                recommendAdapter.clearList();
-                getExpandList();
-            }
-        });
-
-        getExpandList();
+//        recommendWebview.loadUrl("file:///android_asset/discover/discover.html");
+        recommendWebview.loadUrl("http://192.168.0.171/example/discover/discover.html");
     }
 
-    private void getExpandList() {
-        if (!Utils.isNetUsable(getActivity())) {
-            ((BaseActivity) getActivity()).showSnackbar(getResources().getString(R.string.net_unusable));
-            return;
-        }
-        mPresenter.getExpandList("000000", pageIndex, PAGE_SIZE, SharedPreferencesUtils.getString(getActivity(), "ddw_token_type", "") + " " + SharedPreferencesUtils.getString(getActivity(), "ddw_access_token", ""));
-    }
+//    private void getExpandList() {
+//        if (!Utils.isNetUsable(getActivity())) {
+//            ((BaseActivity) getActivity()).showSnackbar(getResources().getString(R.string.net_unusable));
+//            return;
+//        }
+//        mPresenter.getExpandList("000000", pageIndex, PAGE_SIZE, SharedPreferencesUtils.getString(getActivity(), "ddw_token_type", "") + " " + SharedPreferencesUtils.getString(getActivity(), "ddw_access_token", ""));
+//    }
 
     @Override
     public void onResume() {
@@ -138,35 +134,20 @@ public class RecommendFragment extends BaseFragment implements RecommendContract
         MobclickAgent.onPageEnd(TAG);
     }
 
-    @Override
-    public void onHiddenChanged(boolean hidden) {
-        super.onHiddenChanged(hidden);
-    }
-
-    @Optional
-    @OnClick({R.id.title_right_operation_layout})
-    void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.title_right_operation_layout:
-                ((MainActivity) getActivity()).backToFindMoney();
-                break;
-        }
-    }
-
-    @Override
-    public void setPresenter(RecommendContract.RecommendPresenter presenter) {
-        mPresenter = presenter;
-    }
-
-    @Override
-    public void onGetExpandList(List<ExpandListRequestResponse> expands) {
-        pageIndex++;
-        if (expands.size() < PAGE_SIZE) {
-            recommendListRV.disableLoadmore();
-        }
-        if (expands.size() <= 0) return;
-        expandList.clear();
-        expandList.addAll(expands);
-        recommendAdapter.appendList(expandList);
-    }
+//    @Override
+//    public void setPresenter(RecommendContract.RecommendPresenter presenter) {
+//        mPresenter = presenter;
+//    }
+//
+//    @Override
+//    public void onGetExpandList(List<ExpandListRequestResponse> expands) {
+//        pageIndex++;
+//        if (expands.size() < PAGE_SIZE) {
+//            recommendListRV.disableLoadmore();
+//        }
+//        if (expands.size() <= 0) return;
+//        expandList.clear();
+//        expandList.addAll(expands);
+//        recommendAdapter.appendList(expandList);
+//    }
 }
