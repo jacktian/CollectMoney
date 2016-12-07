58759331d409a897740a5cb65f5389a3d28dcf76
JacksonLi <libin_cxf@163.com>
2016-12-07 16:13:59 +0800

    第三方登录

--- app/src/main/java/com/yzdsmart/Dingdingwen/register_login/login/LoginActivity.java
+++ app/src/main/java/com/yzdsmart/Dingdingwen/register_login/login/LoginActivity.java
@@ -1,19 +1,13 @@
 package com.yzdsmart.Dingdingwen.register_login.login;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.tencent.mm.sdk.modelmsg.SendAuth;
-import com.tencent.mm.sdk.openapi.IWXAPI;
-import com.tencent.mm.sdk.openapi.WXAPIFactory;
-import com.tencent.tauth.Tencent;
 import com.umeng.analytics.MobclickAgent;
 import com.yzdsmart.Dingdingwen.App;
 import com.yzdsmart.Dingdingwen.BaseActivity;
@@ -63,119 +57,8 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginVi
     EditText userPasswordET;
 
     private static final String TAG = "LoginActivity";
-    private static final int MSG_USER_ID_FOUND = 1;
-    private static final int MSG_LOGIN = 2;
-    private static final int MSG_AUTH_CANCEL = 3;
-    private static final int MSG_AUTH_ERROR = 4;
-    private static final int MSG_AUTH_COMPLETE = 5;
-
-    //腾讯开始
-    public static final String mTecentAppid = "1105651765";
-    public static Tencent mTencent;
-    //腾讯结束
-
-    //微博开始
-//    /**
-//     * 当前应用的 APP_KEY，第三方应用应该使用自己的 APP_KEY 替换该 APP_KEY
-//     */
-//    public static final String APP_KEY = "1371063102";
-//
-//    /**
-//     * 当前应用的回调页，第三方应用可以使用自己的回调页。
-//     * <p>
-//     * <p>
-//     * 注：关于授权回调页对移动客户端应用来说对用户是不可见的，所以定义为何种形式都将不影响，
-//     * 但是没有定义将无法使用 SDK 认证登录。
-//     * 建议使用默认回调页：https://api.weibo.com/oauth2/default.html
-//     * </p>
-//     */
-//    public static final String REDIRECT_URL = "https://api.weibo.com/oauth2/default.html";
-//
-//    /**
-//     * Scope 是 OAuth2.0 授权机制中 authorize 接口的一个参数。通过 Scope，平台将开放更多的微博
-//     * 核心功能给开发者，同时也加强用户隐私保护，提升了用户体验，用户在新 OAuth2.0 授权页中有权利
-//     * 选择赋予应用的功能。
-//     * <p>
-//     * 我们通过新浪微博开放平台-->管理中心-->我的应用-->接口管理处，能看到我们目前已有哪些接口的
-//     * 使用权限，高级权限需要进行申请。
-//     * <p>
-//     * 目前 Scope 支持传入多个 Scope 权限，用逗号分隔。
-//     * <p>
-//     * 有关哪些 OpenAPI 需要权限申请，请查看：http://open.weibo.com/wiki/%E5%BE%AE%E5%8D%9AAPI
-//     * 关于 Scope 概念及注意事项，请查看：http://open.weibo.com/wiki/Scope
-//     */
-//    public static final String SCOPE =
-//            "email,direct_messages_read,direct_messages_write,"
-//                    + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
-//                    + "follow_app_official_microblog," + "invitation_write";
-//
-//    /**
-//     * 授权认证所需要的信息
-//     */
-//    private AuthInfo mAuthInfo;
-//
-//    /**
-//     * 封装了 "access_token"，"expires_in"，"refresh_token"，并提供了他们的管理功能
-//     */
-//    private Oauth2AccessToken mAccessToken;
-//
-//    /**
-//     * SSO 授权认证实例
-//     */
-//    private SsoHandler mSsoHandler;
-//
-//    /**
-//     * 登陆认证对应的listener
-//     */
-//    private AuthListener mLoginListener = new AuthListener();
-    //微博结束
-
-    //微信开始
-    private static final String APP_ID = "wx1f0757f65d97d285";
-    //IWXAPI是第三方app和微信通信的openapi接口
-    private IWXAPI iwxapi;
-
-    //注册到微信
-    private void regToWx() {
-        //通过WXAPIFactory工厂获取IWXAPI的实例
-        iwxapi = WXAPIFactory.createWXAPI(this, APP_ID, true);
-        iwxapi.registerApp(APP_ID);
-        SendAuth.Req req = new SendAuth.Req();
-        req.scope = "snsapi_userinfo";
-        iwxapi.sendReq(req);
-    }
-    //微信结束
 
     private LoginContract.LoginPresenter mPresenter;
-
-    private Handler authLoginHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what) {
-                case MSG_USER_ID_FOUND: {
-                    showSnackbar("-------用户信息已存在，正在跳转登录操作…--------");
-                }
-                break;
-                case MSG_LOGIN: {
-                    showSnackbar("-------使用" + msg.obj + "帐号登录中…--------");
-                }
-                break;
-                case MSG_AUTH_CANCEL: {
-                    showSnackbar("-------MSG_AUTH_CANCEL--------");
-                }
-                break;
-                case MSG_AUTH_ERROR: {
-                    showSnackbar("-------MSG_AUTH_ERROR--------");
-                }
-                break;
-                case MSG_AUTH_COMPLETE: {
-                    showSnackbar("--------MSG_AUTH_COMPLETE-------");
-                }
-                break;
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -191,15 +74,6 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginVi
         MobclickAgent.openActivityDurationTrack(false);
 
         ShareSDK.initSDK(this);
-
-        //腾讯
-//        if (null == mTencent) {
-//            mTencent = Tencent.createInstance(mTecentAppid, App.getAppInstance());
-//        }
-
-        //微博
-        // 创建授权认证信息
-//        mAuthInfo = new AuthInfo(this, APP_KEY, REDIRECT_URL, SCOPE);
     }
 
     @Override
@@ -261,8 +135,6 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginVi
                 mPresenter.userLogin(userNameET.getText().toString(), userPasswordET.getText().toString(), "", SharedPreferencesUtils.getString(this, "ddw_token_type", "") + " " + SharedPreferencesUtils.getString(this, "ddw_access_token", ""));
                 break;
             case R.id.platform_wechat:
-//                regToWx();
-//                ShareSDK.initSDK(this);
                 Platform platformWeChat = ShareSDK.getPlatform(this, Wechat.NAME);
                 platformWeChat.SSOSetting(false);  //设置false表示使用SSO授权方式
                 platformWeChat.setPlatformActionListener(new PlatformActionListener() {
@@ -290,14 +162,9 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginVi
 
                     }
                 }); // 设置分享事件回调
-//                platformWeChat.authorize();
                 platformWeChat.showUser(null);//授权并获取用户信息
                 break;
             case R.id.platform_qq:
-//                if (!mTencent.isSessionValid()) {
-//                    mTencent.login(this, "all", tecentLoginListener);
-//                }
-//                ShareSDK.initSDK(this);
                 Platform platformQQ = ShareSDK.getPlatform(this, QQ.NAME);
                 platformQQ.SSOSetting(false);  //设置false表示使用SSO授权方式
                 platformQQ.setPlatformActionListener(new PlatformActionListener() {
@@ -325,19 +192,9 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginVi
 
                     }
                 }); // 设置分享事件回调
-//                platformQQ.authorize();
                 platformQQ.showUser(null);//授权并获取用户信息
                 break;
             case R.id.platform_webo:
-//                if (null == mSsoHandler && mAuthInfo != null) {
-//                    mSsoHandler = new SsoHandler(this, mAuthInfo);
-//                }
-//                if (mSsoHandler != null) {
-//                    mSsoHandler.authorize(mLoginListener);
-//                } else {
-//                    showSnackbar("请首先设置微博认证信息");
-//                }
-//                ShareSDK.initSDK(this);
                 Platform platformWeiBo = ShareSDK.getPlatform(this, SinaWeibo.NAME);
                 platformWeiBo.SSOSetting(false);  //设置false表示使用SSO授权方式
                 platformWeiBo.setPlatformActionListener(new PlatformActionListener() {
@@ -365,7 +222,6 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginVi
                         showSnackbar("weibo cancel");
                     }
                 }); // 设置分享事件回调
-//                platformWeiBo.authorize();
                 platformWeiBo.showUser(null);//授权并获取用户信息
                 break;
         }
@@ -376,20 +232,6 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginVi
         super.onStop();
         mPresenter.unRegisterSubscribe();
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (requestCode == Constants.REQUEST_LOGIN) {
-//            Tencent.onActivityResultData(requestCode, resultCode, data, tecentLoginListener);
-//        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-//    private void getUserInfo() {
-//        QQToken qqToken = mTencent.getQQToken();
-//        UserInfo info = new UserInfo(App.getAppInstance(), qqToken);
-//        info.getUserInfo(tecentInfoListener);
-//    }
 
     @Override
     public void setPresenter(LoginContract.LoginPresenter presenter) {
@@ -414,94 +256,6 @@ public class LoginActivity extends BaseActivity implements LoginContract.LoginVi
         setResult(RESULT_OK);
         closeActivity();
     }
-
-//    IUiListener tecentLoginListener = new BaseUiListener() {
-//        @Override
-//        protected void doComplete(JSONObject values) {
-//            try {
-//                int ret = values.getInt("ret");
-//                if (ret == 0) {
-//                    String openID = values.getString("openid");
-//                    String accessToken = values.getString("access_token");
-//                    String expires = values.getString("expires_in");
-//                    mTencent.setOpenId(openID);
-//                    mTencent.setAccessToken(accessToken, expires);
-//                    getUserInfo();
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//
-////            initOpenidAndToken(values);
-////            updateUserInfo();
-////            updateLoginButton();
-//        }
-//    };
-
-
-//    IUiListener tecentInfoListener = new BaseUiListener() {
-//        @Override
-//        protected void doComplete(JSONObject values) {
-//            showSnackbar("Author_Info_SDK:" + values);
-//            if (null != mTencent) {
-//                mTencent.logout(App.getAppInstance());
-//            }
-////            initOpenidAndToken(values);
-////            updateUserInfo();
-////            updateLoginButton();
-//        }
-//    };
-//
-//    private class BaseUiListener implements IUiListener {
-//
-//        @Override
-//        public void onComplete(Object response) {
-//            if (null == response) {
-//                showSnackbar("返回为空,登录失败");
-//                return;
-//            }
-//            JSONObject jsonResponse = (JSONObject) response;
-//            if (null != jsonResponse && jsonResponse.length() == 0) {
-//                showSnackbar("返回为空,登录失败");
-//                return;
-//            }
-//            doComplete((JSONObject) response);
-//        }
-//
-//        protected void doComplete(JSONObject values) {
-//
-//        }
-//
-//        @Override
-//        public void onError(UiError e) {
-//            showSnackbar("异常: " + e.errorDetail);
-//        }
-//
-//        @Override
-//        public void onCancel() {
-//            showSnackbar("取消");
-//        }
-//    }
-
-//    /**
-//     * 登入按钮的监听器，接收授权结果。
-//     */
-//    private class AuthListener implements WeiboAuthListener {
-//        @Override
-//        public void onComplete(Bundle values) {
-//            mAccessToken = Oauth2AccessToken.parseAccessToken(values);
-//        }
-//
-//        @Override
-//        public void onWeiboException(WeiboException e) {
-//            showSnackbar(e.getMessage());
-//        }
-//
-//        @Override
-//        public void onCancel() {
-//            showSnackbar(getResources().getString(R.string.weibosdk_demo_toast_auth_canceled));
-//        }
-//    }
 
     // 这是来自 JPush Example 的设置别名的 Activity 里的代码。一般 App 的设置的调用入口，在任何方便的地方调用都可以。
     private void setAlias(String alias) {
